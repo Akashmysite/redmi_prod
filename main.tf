52b6a26688b5c072e45880fa62ecea48c5271939
akash <akash@gmail.com>
2023-12-07 13:07:29 +0000

    remote access SG created

--- main.tf
+++ main.tf
@@ -42,4 +42,32 @@ resource "aws_security_group" "site-access" {
 
 }
 
+resource "aws_security_group" "remote-access" {
+
+  name        = "${var.project_name}-${var.project_env}-remote-access"
+  description = "${var.project_name}-${var.project_env}-remote-access"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+  tags = {
+    Name = "${var.project_name}-${var.project_env}-remote-access"
+  }
+
+}
+
 
