a4704132c540fefca9f5289854b941fa090b0445
akash <akash@gmail.com>
2023-12-07 13:57:11 +0000

    monitoring port 8080  configured

--- main.tf
+++ main.tf
@@ -56,6 +56,17 @@ resource "aws_security_group" "remote-access" {
 
   }
 
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+
+
+
   egress {
     from_port        = 0
     to_port          = 0
